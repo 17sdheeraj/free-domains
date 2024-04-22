b3672b6b27169c9ed4cfaa1ca0dd8456aef36dd7
Dheeraj <131849765+17sdheeraj@users.noreply.github.com>
2024-04-22 19:07:36 +0530

    Create dheeraj.is-an.app.js

---
+++ domains/dheeraj.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'For My Projects',
+  domain: 'is-an.app',
+  subdomain: 'dheeraj',
+  owner: {
+    email: 'sdheeraj-freedomains@wingarr.eu.org', // Discord contact, 'official.sdheeraj'/'1152458358176088106'
+  },
+  record: {
+    NS: ['ns1.hostry.com', 'ns2.hostry.com', 'ns3.hostry.com', 'ns4.hostry.com'],
+  },
+  proxy: false,
+})
